262a7a09c3e5e83d939b3111d0cee8d18dff8afd
jrodriguezsanjorge <jrodriguezsanjorge@nautilus29.danielcastelao.org>
2018-10-09 11:28:03 +0200

    inicio ejercicio 1

---
+++ Boletin1-1/src/boletin1/pkg1/Boletin11.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1.pkg1;
+
+/**
+ *
+ * @author jrodriguezsanjorge
+ */
+public class Boletin11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int base = 4 , altura = 3, area ;
+    area= base*altura/2;
+    System.out.println("area = "+area);
+    }
+    
+}
